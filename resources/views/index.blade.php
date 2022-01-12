69fb017d954ba5764c6abfce388315f9bcc07c69
adella-it <adella.dl91@gmail.com>
2022-01-12 15:15:14 +0700

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title', 'Wedding Organizer')
+
+@section('container')
+<div class="contain">
+  <div class="row">
+      <div class="col-10">
+        <h1 class="mt-3">Wedding Organizer</h1>
+      </div> 
+  </div>
+</div>
+@endsection
